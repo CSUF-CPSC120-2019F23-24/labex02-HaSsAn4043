695afe7bcaef911d65f220a6e516a68599b36555
HaSsAn4043 <54484253+HaSsAn4043@users.noreply.github.com>
2019-08-31 13:43:23 -0700

    Update main.cpp

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -5,6 +5,12 @@
 
 int main()
 {
-
+cout << "   *\n";
+	cout << "  ***\n";
+	cout << " *****\n";
+	cout << "*******\n";
+	cout << " *****\n";
+	cout << "  ***\n";
+	cout << "   *\n";
   return 0;
 }
