40f647fb0ad1de253f3e40557c83ebae70db754e
hassan.mubarka <hassan.mubarka@cs104u-6c.acad.fullerton.edu>
2019-08-31 15:41:14 -0700

    restaurant bill

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,2 +1,29 @@
-// Name: 
+// Name:
 // This program calculates the tax and tip on a restaurant bill.
+
+#include <iostream>
+
+int main()
+{
+  double tax, tip, mealcost;
+  int area;
+
+  // Get the tax, tip, and meal cost
+  std::cout << "What is the total " mealcost? ";
+  std::cin >> mealcost;
+  std::cout << "What is the \"tip\" percentage? ";
+  std::cin >> tip;
+  std::cout << "What is the \"tax\" percentage? ";
+  std::cin >> tax;
+
+  // Calculate the tax and tip, as well as the total bill after tax and tip are applied.
+tax amount = tax * mealcost;
+total bill = mealcost + tax + tip;
+tip amount = tip * meal cost;
+
+  // Display the tax amount, tip amount, and total bill.
+  std::cout << "The tax is "<< tax amount <<" dollars " << std::endl;
+std::cout << "The tip is "<< tip amount <<" dollars " << std::endl;
+std::cout << "The total bill is "<< total bill <<" dollars " << std::endl;
+  return 0;
+}
